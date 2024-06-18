37fd55eab85b009588e4053a78e1b1bcd3850412
dangoodd <dangoodds@gmail.com>
2024-06-19 00:11:00 +0300

    add neotree config

--- nvim/lua/dangoodd/plugins/file-tree.lua
+++ nvim/lua/dangoodd/plugins/file-tree.lua
@@ -1,13 +1,19 @@
 function conf()
-    require("nvim-tree").setup()
+    require("neo-tree").setup({
+        window = {
+            position = "right",
+        },
+    })
+    -- vim.cmd([[nnoremap \ :Neotree toggle<cr>]])
 end
 
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
     dependencies = {
+        "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
     },
     config = conf,
 }
